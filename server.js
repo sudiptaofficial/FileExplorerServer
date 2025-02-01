eeda4971ac31863edb872c0dfb07aa30e529e48f
sudiptaofficial <sudiptaroy238@gmail.com>
2025-02-01 12:45:44 +0530

    .

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotEnv = require('dotenv');
 require("./config");
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
+app.use(express.json())
 
-const port = process.env.PORT;
+
+const port = process.env.PORT || 5000;
 
 //Server Connection
 app.listen(port,()=>{
